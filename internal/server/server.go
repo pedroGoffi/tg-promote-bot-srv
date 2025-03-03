62f291839e77094416a51a30c6722b40c84a69ac
Pedro goffi <pedrohgdepauloc@gmail.com>
2025-03-03 06:05:13 -0300

    @pedroGoffi[added new route for health diagnosis]

--- internal/server/server.go
+++ internal/server/server.go
@@ -21,6 +21,7 @@ func StartServer() {
 	// Configurar rotas
 	routes.SetupRoutes(r)
 	r.GET("/", func(c *gin.Context) { c.JSON(200, gin.H{"success": "true"}) })
+	r.POST("/", func(c *gin.Context) { c.JSON(200, gin.H{"success": "true"}) })
 
 	// Rodar servidor
 	port := config.GetServerPort()
